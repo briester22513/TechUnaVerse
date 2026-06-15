b001fc329f6db13f95f8fb3978b10a0146cf5fd3
Bri Alexander <brionnaa2@gmail.com>
2026-06-15 15:24:59 -0400

    added some next.js configs

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  output: "export",   // generates a static /out folder — no server needed
+};
 
 export default nextConfig;
